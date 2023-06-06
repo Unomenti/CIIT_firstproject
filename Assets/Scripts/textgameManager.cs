4460a68a4ffc83dc60bdfb6d0758f09025bf73bf
Unomenti <115216515+Unomenti@users.noreply.github.com>
2023-06-06 14:34:14 +0800

    frostbite build 1 - ui updt.
    yeah mhm it says on the tin

--- Assets/Scripts/textgameManager.cs
+++ Assets/Scripts/textgameManager.cs
@@ -10,14 +10,13 @@ public class textgameManager : MonoBehaviour
     public TextMeshProUGUI storyTextMeshPro, hpTextMeshPro, staTextMeshPro;
     public string storyText;
     public int hpValue, staValue;
-    public GameObject branch01;
-    public GameObject branch02;
+    public GameObject health, stamina, mainmenu, branch01, branch02;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-}
+        storyText = "Frostbite";
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,10 +26,25 @@ public class textgameManager : MonoBehaviour
         staTextMeshPro.text = staValue.ToString();
     }
 
+    // main menu buttons
+    public void Begin()
+    {
+        mainmenu.SetActive(false);
+        health.SetActive(true);
+        stamina.SetActive(true);
+        branch01.SetActive(true);
+        storyText = "You stand in an open field, with no paths to walk. The dense fog and the shear coldness of the blizzard obstructs your sense of sight and touch.\n\nAhead, you hear sounds of war. Steel meets flesh. Voices cry out triumph, and anguish. You think if it's all in your head. Still, you chose to flee.";
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
     // One of the three gets called by their respective buttons. You know which is which, they share the same name after all, silly >:v
     public void West()
     {
-        storyText = "Left foot first, facing your left. You start running west, hoping that it'll lead you somewhere. The fog hasn't cleared, but your body gets warmer. Is it the air, or the fatigue from your body? Before you get the chance to answer that question, you suddenly fall from a drop.";
+        storyText = "Left foot first, facing west. You start running, hoping that it'll lead you somewhere. The fog hasn't cleared, but your body gets warmer. Is it the air, or the fatigue from your body?\n\nBefore you get the chance to answer that question, you suddenly fall from a drop. ";
         hpValue -= 1;
         staValue -= 2;
 
@@ -39,7 +53,7 @@ public class textgameManager : MonoBehaviour
 
     public void South()
     {
-        storyText = "You backpedal for a bit before quickly turning around into a full sprint south. The cold air makes your movement stiff, but you press on, as each step gradually gets heavier, and heavier. Eventually, you hear a river ahead of you. You take a breather from what felt like a marathon.";
+        storyText = "You backpedal for a bit before quickly turning around into a full sprint south. The cold air makes your movement stiff, but you press on, as each step gradually gets heavier, and heavier.\n\nEventually, you hear a river ahead of you. You take a breather from what felt like a marathon.";
         staValue -= 7;
 
         branch01.SetActive(false);
